32c1face49a1c42132f7cffa4d75af6527c6a3fb
alexalienware <alexrickett@ucla.edu>
2019-03-30 22:01:56 -0700

    generalize fileselectorbutton, use for games and joytokey directory

--- UnityProject/Assets/FileSelectorButton.cs
+++ UnityProject/Assets/FileSelectorButton.cs
@@ -10,14 +10,25 @@ public class FileSelectorButton : MonoBehaviour
     public enum BaseDirectoryType
     {
         JoyToKey,
-        GamesDirectory
+        GamesDirectory,
+        Other,
     }
 
+    public enum DialogSelectionType
+    {
+        SingleFile,
+        SingleFolder
+    }
+
+    public DialogSelectionType _dialogTarget = DialogSelectionType.SingleFile;
     public BaseDirectoryType _baseDirectory;
     public string windowTitle = "";
     public string startDirectory = null;
     public string extensions = "*";
     InputField _targetField;
+
+    public bool _setFileNameInsteadOfFullPath = true;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -35,14 +46,45 @@ public class FileSelectorButton : MonoBehaviour
     {
         if (string.IsNullOrEmpty(startDirectory) || !Directory.Exists(startDirectory))
         {
-            startDirectory = _baseDirectory == BaseDirectoryType.GamesDirectory ? SwitcherSettings.GamesFolder : SwitcherSettings.JoyToKeyFolder;
+            if (_baseDirectory == BaseDirectoryType.GamesDirectory)
+            {
+                startDirectory = SwitcherSettings.GamesFolder;
+            }
+            else if (_baseDirectory == BaseDirectoryType.JoyToKey)
+            {
+                startDirectory = SwitcherSettings.JoyToKeyFolder;
+            }
+            else if (_baseDirectory == BaseDirectoryType.Other)
+            {
+                startDirectory = Path.GetDirectoryName(Application.dataPath);
+            }
+            
+                
         }
 
-        string result = FileBrowser.OpenSingleFile(windowTitle, startDirectory, extensions);
+        string result = null;
+        if (_dialogTarget == DialogSelectionType.SingleFile)
+        {
+            result = FileBrowser.OpenSingleFile(windowTitle, startDirectory, extensions);
+        }
+        else if (_dialogTarget == DialogSelectionType.SingleFolder)
+        {
+            result = FileBrowser.OpenSingleFolder(windowTitle, startDirectory);
+        }
+
         if (!string.IsNullOrEmpty(result))
         {
             startDirectory = Path.GetDirectoryName(result);
-            _targetField.text = Path.GetFileName(result);//exclude the path
+            if (_setFileNameInsteadOfFullPath)
+            {
+                _targetField.text = Path.GetFileName(result);//exclude the path
+            }
+            else
+            {
+                _targetField.text = result;//exclude the path
+            }
+
+            
         }
     }
 }
