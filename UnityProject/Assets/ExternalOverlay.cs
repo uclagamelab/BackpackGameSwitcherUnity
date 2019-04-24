c97cd52f07cc0b5215b6c6f0a7cd25c6e813fc0f
alexrickett <alexrickett@ucla.edu>
2019-04-23 18:19:37 -0700

    Add scaffolding for showing rainmeter overlay (rainmeter project not yet committed)

---
+++ UnityProject/Assets/ExternalOverlay.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using Debug = UnityEngine.Debug;
+
+public class ExternalOverlay : MonoBehaviour {
+
+    static void ShowOverlay()
+    {
+        //"C:\Program Files\Rainmeter\Rainmeter.exe"[!ActivateConfig "CrockoDial\Main" "Main.ini"][!Move "448" "0"][!Draggable 0]
+        ProcessStartInfo startInfo = new ProcessStartInfo("C:\\Program Files\\Rainmeter\\Rainmeter.exe");
+        startInfo.Arguments ="[!ActivateConfig \"CrockoDial\\Main\" \"Main.ini\"][!Move \"448\" \"0\"][!Draggable 0]";
+        //startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+
+        Process process = new System.Diagnostics.Process();
+        process.StartInfo = startInfo;
+        process.Start();
+    }
+
+    static void HideOverlay()
+    {
+        //"C:\Program Files\Rainmeter\Rainmeter.exe"[!ActivateConfig "CrockoDial\Main" "Main.ini"][!Move "448" "0"][!Draggable 0]
+        ProcessStartInfo startInfo = new ProcessStartInfo("C:\\Program Files\\Rainmeter\\Rainmeter.exe");
+        startInfo.Arguments = "[!DeactivateConfig \"CrockoDial\\Main\" \"Main.ini\"]";
+        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+
+        Process process = new System.Diagnostics.Process();
+        process.StartInfo = startInfo;
+        process.Start();
+    }
+    bool showed = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            showed = !showed;
+            if (showed)
+            {
+                ShowOverlay();
+            }
+            else
+            {
+                HideOverlay();
+            }
+            
+        }
+    }
+}
