6a03f87060864bb37dd521734d215e42dc208804
alexrickett <alexrickett@ucla.edu>
2019-05-02 14:02:55 -0700

    Unity based splash overlay test

--- CrockoSplash/CrockoSplash/Form1.cs
+++ CrockoSplash/CrockoSplash/Form1.cs
@@ -7,14 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace CrockoSplash
 {
     public partial class CrockoSplashSplash : Form
     {
+        [DllImport("User32.dll")]
+        static extern bool MoveWindow(IntPtr handle, int x, int y, int width, int height, bool redraw);
+
+        internal delegate int WindowEnumProc(IntPtr hwnd, IntPtr lparam);
+        [DllImport("user32.dll")]
+        internal static extern bool EnumChildWindows(IntPtr hwnd, WindowEnumProc func, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern int SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        private Process process;
+        private IntPtr unityHWND = IntPtr.Zero;
+
+        private const int WM_ACTIVATE = 0x0006;
+        private readonly IntPtr WA_ACTIVE = new IntPtr(1);
+        private readonly IntPtr WA_INACTIVE = new IntPtr(0);
+
+
         public CrockoSplashSplash()
         {
             InitializeComponent();
+
+            try
+            {
+                process = new Process();
+                process.StartInfo.FileName = "Child.exe";
+                process.StartInfo.Arguments = "-parentHWND " + panel1.Handle.ToInt32() + " " + Environment.CommandLine;
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.Start();
+
+                process.WaitForInputIdle();
+                // Doesn't work for some reason ?!
+                //unityHWND = process.MainWindowHandle;
+                EnumChildWindows(panel1.Handle, WindowEnum, IntPtr.Zero);
+
+                unityHWNDLabel.Text = "Unity HWND: 0x" + unityHWND.ToString("X8");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ".\nCheck if Container.exe is placed next to Child.exe.");
+            }
         }
     }
 }
