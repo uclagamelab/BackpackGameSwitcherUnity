d7f985ff8b11ed35b098c70808e37f0890760374
alex rickett <ack.vandal@gmail.com>
2019-05-03 17:48:20 -0700

    hide cursor when appropriate

---
+++ UnityProject/Assets/CursorManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    bool lastAppliedViz = true;
+    // Update is called once per frame
+    void Update()
+    {
+        bool shouldBeVisible = CommandLineArguments.AdminMode || ToolsAndSettingsMenu.isOpen;
+        if (shouldBeVisible != lastAppliedViz)
+        {
+            Cursor.visible = shouldBeVisible;
+            lastAppliedViz = shouldBeVisible;
+        }
+
+    }
+}
