41dd6f5411263e52369798d7800ff9c69954469c
alexrickett <alexrickett@ucla.edu>
2019-05-30 21:31:43 -0700

    process killer that works

--- Administration/clean_up_processes.bat
+++ Administration/clean_up_processes.bat
@@ -1,6 +1,18 @@
 @echo off
-REM https://superuser.com/questions/1129712/kill-all-processes-with-taskkill-by-username-but-exclude-some-processes
-taskkill /f /t ^
-/fi "IMAGENAME ne chrome.exe" ^
+:https://superuser.com/questions/1129712/kill-all-processes-with-taskkill-by-username-but-exclude-some-processes
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+:------------------------
+taskkill /f ^
+/fi "USERNAME eq %username%" ^
 /fi "IMAGENAME ne explorer.exe" ^
-/fi "IMAGENAME ne sublime_text.exe"
+/fi "USERNAME ne NT AUTHORITY\SYSTEM" ^
+/fi "IMAGENAME ne svchost.exe" ^
+/fi "IMAGENAME ne sihost.exe" ^
+/fi "IMAGENAME ne nvcontainer.exe" ^
+/fi "IMAGENAME ne RestartOnCrash.exe" ^
+/fi "IMAGENAME ne cmd.exe" ^
+/fi "IMAGENAME ne conhost.exe"
+
+timeout 1
+START C:\Users\AlexRickett\Desktop\Switcher_5_30_v2\Crocko-Dial.exe
+
