93a56b329919509f35576ed3f285488505375050
alexrickett <alexrickett@ucla.edu>
2019-05-31 21:45:57 -0700

    cleaner, maybe doesn't work with ue4 though

--- UnityProject/Assets/Scripts/ProcessRunner.cs
+++ UnityProject/Assets/Scripts/ProcessRunner.cs
@@ -76,18 +76,90 @@ public class ProcessRunner : MonoBehaviour
     //https://www.codeproject.com/Questions/413778/Removing-a-Window-Border-No-Winform
     //Import window changing function
     [DllImport("USER32.DLL")]
-    public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    public static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
     [DllImport("user32.dll")]
     static extern bool DrawMenuBar(IntPtr hWnd);
 
     [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-    public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+    public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, uint wFlags);
 
     private const int GWL_STYLE = -16;              //hex constant for style changing
-    private const int WS_BORDER = 0x00800000;       //window with border
-    private const int WS_CAPTION = 0x00C00000;      //window with a title bar
-    private const int WS_SYSMENU = 0x00080000;      //window with no borders etc.
-    private const int WS_MINIMIZEBOX = 0x00020000;  //window with minimizebox
+    const uint WS_BORDER = 0x800000;
+
+    /// <summary>The window has a title bar (includes the WS_BORDER style).</summary>
+    const uint WS_CAPTION = 0xc00000;
+
+    /// <summary>The window is a child window. A window with this style cannot have a menu bar. This style cannot be used with the WS_POPUP style.</summary>
+    const uint WS_CHILD = 0x40000000;
+
+    /// <summary>Excludes the area occupied by child windows when drawing occurs within the parent window. This style is used when creating the parent window.</summary>
+    const uint WS_CLIPCHILDREN = 0x2000000;
+
+    /// <summary>
+    ///   Clips child windows relative to each other; that is, when a particular child window receives a WM_PAINT message, the WS_CLIPSIBLINGS style clips all other overlapping child windows out of the region of the child window to be updated.
+    ///   If WS_CLIPSIBLINGS is not specified and child windows overlap, it is possible, when drawing within the client area of a child window, to draw within the client area of a neighboring child window.
+    /// </summary>
+    const uint WS_CLIPSIBLINGS = 0x4000000;
+
+    /// <summary>The window is initially disabled. A disabled window cannot receive input from the user. To change this after a window has been created, use the EnableWindow function.</summary>
+    const uint WS_DISABLED = 0x8000000;
+
+    /// <summary>The window has a border of a style typically used with dialog boxes. A window with this style cannot have a title bar.</summary>
+    const uint WS_DLGFRAME = 0x400000;
+
+      /// <summary>
+      ///   The window is the first control of a group of controls. The group consists of this first control and all controls defined after it, up to the next control with the WS_GROUP style.
+      ///   The first control in each group usually has the WS_TABSTOP style so that the user can move from group to group. The user can subsequently change the keyboard focus from one control in the group to the next control in the group by using the direction keys.
+      ///   You can turn this style on and off to change dialog box navigation. To change this style after a window has been created, use the SetWindowLong function.
+      /// </summary>
+    const uint WS_GROUP = 0x20000;
+
+    /// <summary>The window has a horizontal scroll bar.</summary>
+    const uint WS_HSCROLL = 0x100000;
+
+    /// <summary>The window is initially maximized.</summary>
+    const uint WS_MAXIMIZE = 0x1000000;
+
+    /// <summary>The window has a maximize button. Cannot be combined with the WS_EX_CONTEXTHELP style. The WS_SYSMENU style must also be specified.</summary>
+    const uint WS_MAXIMIZEBOX = 0x10000;
+
+    /// <summary>The window is initially minimized.</summary>
+    const uint WS_MINIMIZE = 0x20000000;
+
+    /// <summary>The window has a minimize button. Cannot be combined with the WS_EX_CONTEXTHELP style. The WS_SYSMENU style must also be specified.</summary>
+    const uint WS_MINIMIZEBOX = 0x20000;
+
+    /// <summary>The window is an overlapped window. An overlapped window has a title bar and a border.</summary>
+    const uint WS_OVERLAPPED = 0x0;
+
+    /// <summary>The window is an overlapped window.</summary>
+    const uint WS_OVERLAPPEDWINDOW = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_SIZEFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX;
+
+      /// <summary>The window is a pop-up window. This style cannot be used with the WS_CHILD style.</summary>
+    const uint WS_POPUP = 0x80000000u;
+
+      /// <summary>The window is a pop-up window. The WS_CAPTION and WS_POPUPWINDOW styles must be combined to make the window menu visible.</summary>
+    const uint WS_POPUPWINDOW = WS_POPUP | WS_BORDER | WS_SYSMENU;
+
+      /// <summary>The window has a sizing border.</summary>
+    const uint WS_SIZEFRAME = 0x40000;
+
+    /// <summary>The window has a window menu on its title bar. The WS_CAPTION style must also be specified.</summary>
+    const uint WS_SYSMENU = 0x80000;
+
+    /// <summary>
+    ///   The window is a control that can receive the keyboard focus when the user presses the TAB key.
+    ///   Pressing the TAB key changes the keyboard focus to the next control with the WS_TABSTOP style.
+    ///   You can turn this style on and off to change dialog box navigation. To change this style after a window has been created, use the SetWindowLong function.
+    ///   For user-created windows and modeless dialogs to work with tab stops, alter the message loop to call the IsDialogMessage function.
+    /// </summary>
+    const uint WS_TABSTOP = 0x10000;
+
+      /// <summary>The window is initially visible. This style can be turned on and off by using the ShowWindow or SetWindowPos function.</summary>
+      const uint WS_VISIBLE = 0x10000000;
+
+    /// <summary>The window has a vertical scroll bar.</summary>
+    const uint WS_VSCROLL = 0x200000;
 
     public const int SWP_ASYNCWINDOWPOS = 0x4000;
     public const int SWP_DEFERERASE = 0x2000;
@@ -114,31 +186,34 @@ public class ProcessRunner : MonoBehaviour
     public void TESTREMOVEBORDER()
     {
 
-        if (_allWindowsCached.ContainsKey("Artifacts- Phazero"))
+        if (_allWindowsCached.ContainsKey("New Unity Project"))
         {
-            IntPtr window = _allWindowsCached["Artifacts- Phazero"];
+            IntPtr window = _allWindowsCached["New Unity Project"];
             //SetWindowPos(window, 0, 0, 0, 1920, 1080, SWP_SHOWWINDOW);
 
 
-            DrawMenuBar(window);
+            //// --- WORKING FOR OLDER UNITY BUILD
+            SetWindowPos(window, 0, 0, 0, 1920, 1080, SWP_SHOWWINDOW);
+            SetWindowLong(window, GWL_STYLE, WS_POPUP);
+            SetWindowPos(window, 0, 0, 0, 1920, 1080,
+             SWP_NOMOVE | SWP_NOZORDER | SWP_NOSIZE | SWP_FRAMECHANGED | SWP_SHOWWINDOW);
+            ////--------------------------
+            // --- MORE LIK UE4
+
+            //SetWindowLong(window, GWL_STYLE, WS_POPUP);
+            //SetWindowPos(window, 0, 0, 0, 1920, 1080,
+            // SWP_NOMOVE | SWP_NOZORDER | SWP_NOSIZE | SWP_FRAMECHANGED | SWP_SHOWWINDOW);
             //this.delayedFunction(() =>
-            //{
-                SetWindowLong(window, GWL_STYLE, WS_SYSMENU);
-                SetWindowPos(window, 0, 0, 0, 1920, 1080,
-                    SWP_NOMOVE | SWP_NOZORDER | SWP_NOSIZE | SWP_FRAMECHANGED | SWP_SHOWWINDOW);
+            //    {
+            //        SetWindowPos(window, 0, 0, 0, 1920, 1080, SWP_FRAMECHANGED);
+            //    }, 1);
 
-            //}
-            //, 1);
+            //--------------------------
 
-            this.delayedFunction(() =>
-            {
-                SetWindowPos(window, 0, 0, 0, 1920, 1080, SWP_SHOWWINDOW);
-            }
-          , 1);
 
 
         }
-        
+
     }
 
     // cycles through every window and calls callback
