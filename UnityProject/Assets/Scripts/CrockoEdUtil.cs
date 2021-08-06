59033cd0c457eb3f8858be6aea0afffec6f3e1f4
Alex Rickett <ack.vandal@gmail.com>
2021-08-06 00:39:13 -0700

    Show version # add menu item to update version (generated from date) some layout tweaks

---
+++ UnityProject/Assets/Scripts/CrockoEdUtil.cs
@@ -0,0 +1,16 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using DateTime = System.DateTime;
+
+public static class CrockoEdUtil 
+{
+    [MenuItem("CrockoDial/Update Verision")]
+    public static void UpdateVersion()
+    {
+        PlayerSettings.bundleVersion = $"1.{ (DateTime.Now.Year % 100).ToString("00")}.{DateTime.Now.Month}.{DateTime.Now.Day}";
+    }
+}
+#endif
---
+++ UnityProject/Assets/Scripts/VersionDisplayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionDisplayer : MonoBehaviour
+{
+    void Start()
+    {
+        this.GetComponent<Text>().text = "v"+Application.version;   
+    }
+
+
+}
