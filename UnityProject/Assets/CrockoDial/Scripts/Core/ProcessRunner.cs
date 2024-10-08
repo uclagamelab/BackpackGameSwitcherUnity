3147e862dc4c87861da10844affab961dab316f4
AlexR <alexrickett@ucla.edu>
2024-10-08 00:06:51 -0700

    disable joy to key when in keyboard mode misc clean up

--- UnityProject/Assets/CrockoDial/Scripts/Core/ProcessRunner.cs
+++ UnityProject/Assets/CrockoDial/Scripts/Core/ProcessRunner.cs
@@ -24,15 +24,14 @@ using Microsoft.Win32;
 
 public class ProcessRunner : MonoBehaviour
 {
-    public string processStateHelper;
-
-    public static bool SwitcherAppHasFocus = true;
+    public static bool SwitcherAppHasFocus { get; private set; } = true;
 
     public class EventCallbacks
     {
         public System.Action OnProcessLaunched = () => { };
         public System.Action OnProcessExited = () => { };
     }
+
     public static EventCallbacks Events = new EventCallbacks();
 
     //Add startup forgiveness timer, for games that change window
@@ -48,52 +47,10 @@ public class ProcessRunner : MonoBehaviour
 
     static XUSingleTown<ProcessRunner> _instanceHelper = new XUSingleTown<ProcessRunner>();
     public static ProcessRunner instance => _instanceHelper.instance;
-    // DLL Imports
-
 
     float currentProcessStartTimer = 0;
 
-
     List<Process> safeProcesses;
-
-
-    /*[ContextMenu("TESTREMOVE")]
-    public void TESTREMOVEBORDER()
-    {
-
-        if (_allWindowsCached.ContainsKey("New Unity Project"))
-        {
-            IntPtr window = _allWindowsCached["New Unity Project"];
-            //SetWindowPos(window, 0, 0, 0, 1920, 1080, SWP_SHOWWINDOW);
-
-
-            //// --- WORKING FOR OLDER UNITY BUILD
-            SetWindowPos(window, 0, 0, 0, 1920, 1080, SWP_SHOWWINDOW);
-            SetWindowLong(window, GWL_STYLE, WS_POPUP);
-            SetWindowPos(window, 0, 0, 0, 1920, 1080,
-             SWP_NOMOVE | SWP_NOZORDER | SWP_NOSIZE | SWP_FRAMECHANGED | SWP_SHOWWINDOW);
-            ////--------------------------
-            // --- MORE LIK UE4
-
-            //SetWindowLong(window, GWL_STYLE, WS_POPUP);
-            //SetWindowPos(window, 0, 0, 0, 1920, 1080,
-            // SWP_NOMOVE | SWP_NOZORDER | SWP_NOSIZE | SWP_FRAMECHANGED | SWP_SHOWWINDOW);
-            //this.delayedFunction(() =>
-            //    {
-            //        SetWindowPos(window, 0, 0, 0, 1920, 1080, SWP_FRAMECHANGED);
-            //    }, 1);
-
-            //--------------------------
-
-
-
-        }
-
-    }
-
-    */
-
-
 
     Process _thisProcess = Process.GetCurrentProcess(); //The application switcher process?
     GameData _runningGame = null;
@@ -101,19 +58,22 @@ public class ProcessRunner : MonoBehaviour
     //Process _currentRunningGameProcess = null; //the currently running game process
     Process _joy2KeyProcess = null;
 
-
-
     IntPtr _joy2KeyPrimaryWindow = IntPtr.Zero;
-
-
 
     System.Text.StringBuilder _sb = new System.Text.StringBuilder();
 
     // Pressing the button opens up the game.
     // Pressing CTRL - C brings this game to the foreground
 
+    bool _usingJoyToKey = true;
+
     IEnumerator Start()
     {
+        if (SwitcherSettings.Data._controlMode == CrockoInputMode.mouseAndKeyboard)
+        {
+            _usingJoyToKey = false;
+        }
+
         determineSwitcherJoyToKeyConfigFile();
         setJoyToKeyConfig(switcherJoyToKeyConfig);
 
@@ -128,7 +88,6 @@ public class ProcessRunner : MonoBehaviour
             SetWindowsExplorerRunning(false);
 #endif
         }
-
     }
 
     private void Update()
@@ -293,25 +252,8 @@ public class ProcessRunner : MonoBehaviour
     then you can save that window.  
     */
 
-
-    // Finds the foreground window for the given bucket. If there isn't one, returns IntPtr.zero
-    IntPtr GetForegroundWindowFrom(List<IntPtr> bucket)
-    {
-        IntPtr fgWnd = GetForegroundWindow();
-
-        if (bucket.Contains(fgWnd))
-        {
-            return fgWnd;
-        }
-
-        return IntPtr.Zero;
-    }
-
-
-
     public void BringThisToForeground()
     {
-
         setJoyToKeyConfigIfNotAlreadySet(switcherJoyToKeyConfig);
         string switcherWindowName = Application.productName;
 
@@ -368,7 +310,6 @@ public class ProcessRunner : MonoBehaviour
         process.Start();
     }
 
-
     static System.Text.StringBuilder _mousePosCommandBuilderCmdBuilder = new System.Text.StringBuilder();
     public static void SetMousePosition(int x, int y, bool absolute = true)
     {
@@ -409,11 +350,8 @@ public class ProcessRunner : MonoBehaviour
         process.Start();
     }
 
-
-
     public void StopCurrentRunningGame()
     {
-
         if (currentGameRunner != null)
         {
             //UnityEngine.Debug.Log("CloseProcess called : " + _currentRunningGameProcess.Id);
@@ -423,8 +361,6 @@ public class ProcessRunner : MonoBehaviour
         currentGameRunner?.Reset();
         _runningGame = null;
     }
-
-
 
     void recordSafeProcesses()
     {
@@ -498,18 +434,6 @@ public class ProcessRunner : MonoBehaviour
         }
     }
 
-    void killAllPrevProcesses()
-    {
-        Process[] processes = Process.GetProcesses();
-        foreach (Process p in processes)
-        {
-            if (p.Id != this._thisProcess.Id)
-            {
-                WinOsUtil.TerminateProcess((uint)p.Handle, 0);
-            }
-        }
-    }
-
     bool isSafeProcess(Process proc)
     {
         foreach (Process sp in safeProcesses)
@@ -552,16 +476,21 @@ public class ProcessRunner : MonoBehaviour
 
     bool joyToKeyProfileExists(string profile)
     {
-        return 
-            !string.IsNullOrEmpty(profile) 
+        return
+            !string.IsNullOrEmpty(profile)
             &&
-            !string.IsNullOrEmpty(SwitcherSettings.Data.JoyToKeyFolder) 
-            && 
+            !string.IsNullOrEmpty(SwitcherSettings.Data.JoyToKeyFolder)
+            &&
             System.IO.File.Exists(Path.Combine(SwitcherSettings.Data.JoyToKeyFolder, profile));
     }
 
     public void setJoyToKeyConfig(string configFile)
     {
+        if (!_usingJoyToKey)
+        {
+            return;
+        }
+
         _currentJoyToKeyConfig = configFile;
 
         string joyToKeyExe = CompanionSoftware.JoyToKey;
@@ -587,4 +516,35 @@ public class ProcessRunner : MonoBehaviour
     }
     #endregion
 
+
+    /*[ContextMenu("TESTREMOVE")]
+    public void TESTREMOVEBORDER()
+    {
+
+        if (_allWindowsCached.ContainsKey("New Unity Project"))
+        {
+            IntPtr window = _allWindowsCached["New Unity Project"];
+            //SetWindowPos(window, 0, 0, 0, 1920, 1080, SWP_SHOWWINDOW);
+
+
+            //// --- WORKING FOR OLDER UNITY BUILD
+            SetWindowPos(window, 0, 0, 0, 1920, 1080, SWP_SHOWWINDOW);
+            SetWindowLong(window, GWL_STYLE, WS_POPUP);
+            SetWindowPos(window, 0, 0, 0, 1920, 1080,
+             SWP_NOMOVE | SWP_NOZORDER | SWP_NOSIZE | SWP_FRAMECHANGED | SWP_SHOWWINDOW);
+            ////--------------------------
+            // --- MORE LIK UE4
+
+            //SetWindowLong(window, GWL_STYLE, WS_POPUP);
+            //SetWindowPos(window, 0, 0, 0, 1920, 1080,
+            // SWP_NOMOVE | SWP_NOZORDER | SWP_NOSIZE | SWP_FRAMECHANGED | SWP_SHOWWINDOW);
+            //this.delayedFunction(() =>
+            //    {
+            //        SetWindowPos(window, 0, 0, 0, 1920, 1080, SWP_FRAMECHANGED);
+            //    }, 1);
+
+            //--------------------------
+        }
+    }
+    */
 }
